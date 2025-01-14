7f93fe68fc972b4f7f25e73f7d7951ed69179a09
Héctor Wood Santana <hectorwoodsan@gmail.com>
2025-01-14 11:41:43 +0000

    Añadido nuevo método de mover el robot usando MoveSteering

--- script_trabajo.py
+++ script_trabajo.py
@@ -8,16 +8,18 @@ from ev3dev2.led import Leds
 from time import sleep
 import math
 
-# Funcion para transformar distancia en grados
+# Funcion para transformar distancia en grados. En base al diámetro de la rueda del robot
 def distancia_a_grados(distancia):
-    return (distancia / (5.6 * math.pi)) * 360
+    diametro_ruedas = 5.6 # Debe ser calibrado para el diámetro real de la rueda
+    grados = (distancia / (diametro_ruedas * math.pi)) * 360
+    return grados
 
 # Función para el calculo del giro de 90 grados en base a la distancia entre las ruedas y diametro de las ruedas
 # Longitud entre las 2 ruedas = 12 cm
 # Diametro ruedas = 5.6 cm
 # Grados para los giros de 90 grados ~= 192 grados
-def grados_para_giro_90_grados():
-    diametro_ruedas = 5.6
+def grados_para_giro_90_grados(): 
+    diametro_ruedas = 5.6       # Debe ser calibrado para el diámetro real de la rueda
     longitud_entre_ruedas = 12
     return (((math.pi * longitud_entre_ruedas) / 4) / (math.pi * diametro_ruedas)) * 360
 
@@ -38,9 +40,23 @@ class SensorManager:
     
 # Clase para controlar el movimiento
 class MovementController:
-    def __init__(self, left_motor, right_motor):
+    def __init__(self, left_motor, right_motor, steering_drive):
         self.left_motor = left_motor
         self.right_motor = right_motor
+        self.steering_drive = steering_drive
+
+    def move_forward(self, speed, distance_cm):
+        degrees = distancia_a_grados(distance_cm)
+        self.steering_drive.on_for_degrees(0, speed, degrees, brake=True, block=True)
+    
+    def stop(self):
+        self.steering_drive.off()
+
+    def turn_left_steering(self, speed, degrees=grados_para_giro_90_grados()):
+        self.steering_drive.on_for_degrees(-100, speed, degrees, brake=True, block=True)
+
+    def turn_right_steering(self, speed, degrees=grados_para_giro_90_grados()):
+        self.steering_drive.on_for_degrees(100, speed, degrees, brake=True, block=True)
 
     def move_forward(self, speed, distance_cm):
         degrees = distancia_a_grados(distance_cm)
@@ -59,8 +75,9 @@ class MovementController:
         self.left_motor.on_for_degrees(speed, degrees, brake=True, block=False)
         self.right_motor.on_for_degrees(-speed, degrees, brake=True, block=True)
 
+    
 # Inicializar los motores y los sensores
-steering_drive = MoveSteering(OUTPUT_C, OUTPUT_B)
+steering_drive = MoveSteering(OUTPUT_B, OUTPUT_C)
 color_sensor = ColorSensor(INPUT_4)
 ultrasonic_sensor = UltrasonicSensor(INPUT_2)
 sound = Sound()
@@ -68,7 +85,7 @@ leds = Leds()
 # Controlador de los sensores
 sensor_manager = SensorManager(color_sensor, ultrasonic_sensor)
 # Controlador del movimiento
-movement_controller = MovementController(steering_drive.left_motor, steering_drive.right_motor)
+movement_controller = MovementController(steering_drive.left_motor, steering_drive.right_motor, steering_drive)
 
 
 
@@ -85,8 +102,21 @@ def buscar_linea_y_obstaculo(color, threshold):
             
     
 # Función para seguir la linea negra y evitar obstáculos
-def seguir_linea_y_evitar_obstaculos():
-    False
+def maniobra_evitar_obstaculos():
+    movement_controller.turn_left(10)
+    sleep(1)
+    movement_controller.move_forward(10,20)
+    sleep(1)
+    movement_controller.turn_right(10)
+    sleep(1)
+    movement_controller.move_forward(10, 46)
+    sleep(1)
+    movement_controller.turn_right(10)
+    sleep(1)
+    movement_controller.move_forward(10,20)
+    sleep(1)
+    movement_controller.turn_left(10)
+
 
 
 
@@ -96,27 +126,13 @@ def main():
     leds.set_color("LEFT", "RED")
     leds.set_color("RIGHT", "GREEN")
     sound.beep()
-
-    movement_controller.move_forward(30, 30)
-    sleep(1)
-    movement_controller.turn_left(30)
-    sleep(1)
-    movement_controller.move_forward(30, 30)
-    sleep(1)
-    movement_controller.turn_right(30)
-    sleep(1)
-    movement_controller.move_forward(30, 30)
-    sleep(1)
-
-    while True:
-        break
-        color = sensor_manager.get_color()
-        distance = sensor_manager.get_distance()
-
-        #buscar_linea_y_obstaculo(color, threshold=30)
-    
-
-        time.sleep(0.1)  # Small delay to prevent overwhelming the sensors
+    #maniobra_evitar_obstaculos()
+    movement_controller.move_forward(10, 20)
+    movement_controller.turn_left_steering(10)
+    movement_controller.turn_right_steering(10)
+    sleep(5)
+    movement_controller.turn_left(10)
+    movement_controller.turn_right(10)
 
 if __name__ == "__main__":
     main()
