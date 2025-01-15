73c0af22b0d7d19223f4e5d5fd76a939d35d5fe5
Héctor Wood Santana <hectorwoodsan@gmail.com>
2025-01-15 18:24:47 +0000

    Metodos maniobra inicial y clase Movement controller refactorizada

--- script_trabajo.py
+++ script_trabajo.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from ev3dev2.motor import MoveSteering, OUTPUT_C, OUTPUT_B
-#from ev3dev2.sensor import INPUT_2, INPUT_4
 from ev3dev2.sensor.lego import UltrasonicSensor, ColorSensor
 from ev3dev2.sound import Sound
 from ev3dev2.led import Leds
@@ -20,7 +19,7 @@ def distancia_a_grados(distancia):
 # Grados para los giros de 90 grados ~= 192 grados
 def grados_para_giro_90_grados(): 
     diametro_ruedas = 5.6       # Debe ser calibrado para el diámetro real de la rueda
-    longitud_entre_ruedas = 12
+    longitud_entre_ruedas = 12  # Debe ser calibrado para longitud real entre las ruedas
     return (((math.pi * longitud_entre_ruedas) / 4) / (math.pi * diametro_ruedas)) * 360
 
 # Clase para controlar los Sensores
@@ -58,23 +57,6 @@ class MovementController:
     def turn_right_steering(self, speed, degrees=grados_para_giro_90_grados()):
         self.steering_drive.on_for_degrees(100, speed, degrees, brake=True, block=True)
 
-    # Métodos antiguos actuando directamente sobre los motores
-    def move_forward(self, speed, distance_cm):
-        degrees = distancia_a_grados(distance_cm)
-        self.left_motor.on_for_degrees(speed, degrees, brake=True, block=False)
-        self.right_motor.on_for_degrees(speed, degrees, brake=True, block=True)
-
-    def stop(self):
-        self.left_motor.off()
-        self.right_motor.off()
-
-    def turn_left(self, speed, degrees=grados_para_giro_90_grados()):
-        self.left_motor.on_for_degrees(-speed, degrees, brake=True, block=False)
-        self.right_motor.on_for_degrees(speed, degrees, brake=True, block=True)
-
-    def turn_right(self, speed, degrees=grados_para_giro_90_grados()):
-        self.left_motor.on_for_degrees(speed, degrees, brake=True, block=False)
-        self.right_motor.on_for_degrees(-speed, degrees, brake=True, block=True)
 
     
 # Inicializar los motores y los sensores
@@ -91,43 +73,39 @@ movement_controller = MovementController(steering_drive.left_motor, steering_dri
 
 # Función para localizar si el robot está a la derecha o la izquierda de la línea negra y nos deja mirando al obstáculo.
 def maniobra_inicial(detection_range):
-    derecha = False
-    izquierda = False
     deteccion = False
     # Giramos a la izquierda y miramos si hay algún obstaculo mientras giramos
     for i in range(9):
         movement_controller.turn_left_steering(10, degrees=(grados_para_giro_90_grados() / 9))
         if sensor_manager.is_object_detected(detection_range):
-            izquierda = True
             deteccion = True
-            return izquierda, derecha, deteccion
+            return deteccion
     # Volvemos a la posición inicial.
     movement_controller.turn_right_steering(10)
     # Giramos a la derecha y miramos si hay algún obstaculo mientras giramos
     for i in range(9):
         movement_controller.turn_right_steering(10, degrees=(grados_para_giro_90_grados() / 9))
         if sensor_manager.is_object_detected(detection_range):
-            derecha = True
             deteccion = True
-            return izquierda, derecha, deteccion
+            return deteccion
     # Volvemos a la posición inicial.
     movement_controller.turn_left_steering(10)
-    return izquierda, derecha, deteccion
+    return deteccion
     
 
 # Función para seguir la linea negra y evitar obstáculos
 def maniobra_evitar_obstaculos(distancia):
     movement_controller.turn_left_steering(10)
     sleep(0.5)
-    movement_controller.move_forward_steering(10, (distancia + 2.8))
+    movement_controller.move_forward_steering(10, (distancia + 12.8))
     sleep(0.5)
     movement_controller.turn_right_steering(10)
     sleep(0.5)
-    movement_controller.move_forward_steering(10, (distancia + 2.8) * 2 )
+    movement_controller.move_forward_steering(10, (distancia + 2.8) * 2)
     sleep(0.5)
     movement_controller.turn_right_steering(10)
     sleep(0.5)
-    movement_controller.move_forward_steering(10, (distancia + 2.8))
+    movement_controller.move_forward_steering(10, (distancia + 12.8))
     sleep(0.5)
     movement_controller.turn_left_steering(10)
 
@@ -144,7 +122,7 @@ def main():
     
     while deteccion == False:
         # Maniobra inical para saber donde está la linea negra y orientarnos. (Detecta objetos a < N cm)
-        izquierda, derecha, deteccion = maniobra_inicial(70)
+        deteccion = maniobra_inicial(70)
         if deteccion == False:
             movement_controller.move_forward_steering(10, 15)
     
